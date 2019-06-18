0bdc9a1b0bed4aa91493ce4e309ccce0aabbfcc8
juancon <33897333+juancon@users.noreply.github.com>
2019-06-18 19:25:16 +0200

    comentarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,20 +9,20 @@ import {RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
 	//indicamos la ruta que tendra cada componente
-	{path: '**', component: AppComponent},
-	{path: '', component: AppComponent},
+	{path: '**', component: AppComponent},//ruta para todas las demas rutas
+	{path: '', component: AppComponent},//ruta por defecto
 	{path: 'inicio', component: AppComponent},
-	{path: 'inicio/:page', component: AppComponent},
+	{path: 'inicio/:page', component: AppComponent},//indicamos que la ruta puede tener parametros
 	{path: 'login', component: LoginComponent},
-	{path: 'login/:page', component: LoginComponent},
+	{path: 'login/:page', component: LoginComponent},//indicamos que la ruta puede tener parametros
 	{path: 'amigos', component: AppComponent},
 	{path: 'mensajes', component: AppComponent},
 	{path: 'peticiones', component: AppComponent},
 	{path: 'buscar', component: AppComponent},
-	{path: 'buscar/:page', component: AppComponent},
+	{path: 'buscar/:page', component: AppComponent},//indicamos que la ruta puede tener parametros
 	{path: 'usuario', component: AppComponent},
 	{path: 'navegacion', component: AppComponent},
-	{path: 'usuario/:page', component: AppComponent},
+	{path: 'usuario/:page', component: AppComponent},//indicamos que la ruta puede tener parametros
 	{path: 'registro', component: RegistroComponent},
 	{path: 'perfil', component: AppComponent}
 ];
